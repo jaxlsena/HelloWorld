712db36275994c121f3e5515fc049a978935f3f9
John Doe <johndoe@example.com>
2016-07-06 14:31:33 -0400

    first Heroku commit

---
+++ server.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+
+get '/' do
+  "hello world"
+end
+
+get "/sinatra" do
+  "hello sinatra"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# get '/' do
+#   show something
+# end
+#
+# put '/' do
+#   replace something
+# end
+#
+# post '/' do
+#   create something
+# end
+#
+# delete '/' do
+#   delete something
+# end
